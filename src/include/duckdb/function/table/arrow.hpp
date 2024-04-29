75fd9d2ebfd76f111e798d290b74a203c3ea9b8d
Aldrin Montana <octalene.dev@pm.me>
2024-04-29 15:25:52 -0700

    minor: refactored style for my own readability
    This almost certainly won't jive with upstream duckdb, but it'll make it
    easier for me to develop in the short term.

--- src/include/duckdb/function/table/arrow.hpp
+++ src/include/duckdb/function/table/arrow.hpp
@@ -20,219 +20,248 @@
 
 namespace duckdb {
 
-struct ArrowInterval {
-	int32_t months;
-	int32_t days;
-	int64_t nanoseconds;
+  struct ArrowInterval {
+    int32_t months;
+    int32_t days;
+    int64_t nanoseconds;
 
-	inline bool operator==(const ArrowInterval &rhs) const {
-		return this->days == rhs.days && this->months == rhs.months && this->nanoseconds == rhs.nanoseconds;
-	}
-};
+    inline bool operator==(const ArrowInterval &rhs) const {
+      return this->days == rhs.days && this->months == rhs.months && this->nanoseconds == rhs.nanoseconds;
+    }
+  };
 
-struct ArrowProjectedColumns {
-	unordered_map<idx_t, string> projection_map;
-	vector<string> columns;
-	// Map from filter index to column index
-	unordered_map<idx_t, idx_t> filter_to_col;
-};
+  struct ArrowProjectedColumns {
+    unordered_map<idx_t, string> projection_map;
+    vector<string> columns;
+    // Map from filter index to column index
+    unordered_map<idx_t, idx_t> filter_to_col;
+  };
 
-struct ArrowStreamParameters {
-	ArrowProjectedColumns projected_columns;
-	TableFilterSet *filters;
-};
+  struct ArrowStreamParameters {
+    ArrowProjectedColumns projected_columns;
+    TableFilterSet *filters;
+  };
 
-typedef unique_ptr<ArrowArrayStreamWrapper> (*stream_factory_produce_t)(uintptr_t stream_factory_ptr,
-                                                                        ArrowStreamParameters &parameters);
-typedef void (*stream_factory_get_schema_t)(ArrowArrayStream *stream_factory_ptr, ArrowSchema &schema);
+  typedef unique_ptr<ArrowArrayStreamWrapper> (*stream_factory_produce_t)(uintptr_t stream_factory_ptr,
+                                                                          ArrowStreamParameters &parameters);
+  typedef void (*stream_factory_get_schema_t)(ArrowArrayStream *stream_factory_ptr, ArrowSchema &schema);
 
-struct ArrowScanFunctionData : public TableFunctionData {
-public:
-	ArrowScanFunctionData(stream_factory_produce_t scanner_producer_p, uintptr_t stream_factory_ptr_p,
-	                      shared_ptr<DependencyItem> dependency = nullptr)
-	    : lines_read(0), rows_per_thread(0), stream_factory_ptr(stream_factory_ptr_p),
-	      scanner_producer(scanner_producer_p), dependency(std::move(dependency)) {
-	}
+	struct ArrowScanFunctionData : public TableFunctionData {
+		ArrowScanFunctionData( stream_factory_produce_t   scanner_producer_p
+		                      ,uintptr_t                  stream_factory_ptr_p
+		                      ,shared_ptr<DependencyItem> dependency = nullptr)
+		  :  lines_read(0)
+		    ,rows_per_thread(0)
+		    ,stream_factory_ptr(stream_factory_ptr_p)
+		    ,scanner_producer(scanner_producer_p)
+		    ,dependency(std::move(dependency)) {}
 
-	vector<LogicalType> all_types;
-	atomic<idx_t> lines_read;
-	ArrowSchemaWrapper schema_root;
-	idx_t rows_per_thread;
-	//! Pointer to the scanner factory
-	uintptr_t stream_factory_ptr;
-	//! Pointer to the scanner factory produce
-	stream_factory_produce_t scanner_producer;
-	//! The (optional) dependency of this function (used in Python for example)
-	shared_ptr<DependencyItem> dependency;
-	//! Arrow table data
-	ArrowTableType arrow_table;
-	//! Whether projection pushdown is enabled on the scan
-	bool projection_pushdown_enabled = true;
-};
+		vector<LogicalType> all_types;
+		atomic<idx_t>       lines_read;
+		ArrowSchemaWrapper  schema_root;
+		idx_t               rows_per_thread;
 
-struct ArrowRunEndEncodingState {
-public:
-	ArrowRunEndEncodingState() {
-	}
+		//! Pointer to the scanner factory
+		uintptr_t stream_factory_ptr;
 
-public:
-	unique_ptr<Vector> run_ends;
-	unique_ptr<Vector> values;
+		//! Pointer to the scanner factory produce
+		stream_factory_produce_t scanner_producer;
 
-public:
-	void Reset() {
-		run_ends.reset();
-		values.reset();
-	}
-};
+		//! The (optional) dependency of this function (used in Python for example)
+		shared_ptr<DependencyItem> dependency;
 
-struct ArrowScanLocalState;
-struct ArrowArrayScanState {
-public:
-	explicit ArrowArrayScanState(ArrowScanLocalState &state, ClientContext &context);
+		//! Arrow table data
+		ArrowTableType arrow_table;
 
-public:
-	ArrowScanLocalState &state;
-	// Hold ownership over the Arrow Arrays owned by DuckDB to allow for zero-copy
-	shared_ptr<ArrowArrayWrapper> owned_data;
-	unordered_map<idx_t, unique_ptr<ArrowArrayScanState>> children;
-	// Optionally holds the pointer that was used to create the cached dictionary
-	optional_ptr<ArrowArray> arrow_dictionary = nullptr;
-	// Cache the (optional) dictionary of this array
-	unique_ptr<Vector> dictionary;
-	//! Run-end-encoding state
-	ArrowRunEndEncodingState run_end_encoding;
-	ClientContext &context;
+		//! Whether projection pushdown is enabled on the scan
+		bool projection_pushdown_enabled = true;
+	};
 
-public:
-	ArrowArrayScanState &GetChild(idx_t child_idx);
-	void AddDictionary(unique_ptr<Vector> dictionary_p, ArrowArray *arrow_dict);
-	bool HasDictionary() const;
-	bool CacheOutdated(ArrowArray *dictionary) const;
-	Vector &GetDictionary();
-	ArrowRunEndEncodingState &RunEndEncoding() {
-		return run_end_encoding;
-	}
+	struct ArrowRunEndEncodingState {
+	  unique_ptr<Vector> run_ends;
+	  unique_ptr<Vector> values;
 
-public:
-	void Reset() {
-		// Note: dictionary is not reset
-		// the dictionary should be the same for every array scanned of this column
-		run_end_encoding.Reset();
-		for (auto &child : children) {
-			child.second->Reset();
-		}
-		owned_data.reset();
-	}
-};
+	  ArrowRunEndEncodingState() {}
 
-struct ArrowScanLocalState : public LocalTableFunctionState {
-public:
-	explicit ArrowScanLocalState(unique_ptr<ArrowArrayWrapper> current_chunk, ClientContext &context)
-	    : chunk(current_chunk.release()), context(context) {
-	}
+	  void Reset() {
+	    run_ends.reset();
+	    values.reset();
+	  }
+	};
 
-public:
-	shared_ptr<ArrowArrayWrapper> chunk;
-	idx_t chunk_offset = 0;
-	idx_t batch_index = 0;
-	vector<column_t> column_ids;
-	unordered_map<idx_t, unique_ptr<ArrowArrayScanState>> array_states;
-	TableFilterSet *filters = nullptr;
-	//! The DataChunk containing all read columns (even filter columns that are immediately removed)
-	DataChunk all_columns;
-	ClientContext &context;
+	struct ArrowScanLocalState;
+	struct ArrowArrayScanState {
+		ArrowScanLocalState &state;
 
-public:
-	void Reset() {
-		chunk_offset = 0;
-		for (auto &col : array_states) {
-			col.second->Reset();
-		}
-	}
-	ArrowArrayScanState &GetState(idx_t child_idx) {
-		auto it = array_states.find(child_idx);
-		if (it == array_states.end()) {
-			auto child_p = make_uniq<ArrowArrayScanState>(*this, context);
-			auto &child = *child_p;
-			array_states.emplace(child_idx, std::move(child_p));
-			return child;
-		}
-		return *it->second;
-	}
-};
+		// Hold ownership over the Arrow Arrays owned by DuckDB to allow for zero-copy
+		shared_ptr<ArrowArrayWrapper> owned_data;
+		unordered_map<idx_t, unique_ptr<ArrowArrayScanState>> children;
 
-struct ArrowScanGlobalState : public GlobalTableFunctionState {
-	unique_ptr<ArrowArrayStreamWrapper> stream;
-	mutex main_mutex;
-	idx_t max_threads = 1;
-	idx_t batch_index = 0;
-	bool done = false;
+		// Optionally holds the pointer that was used to create the cached dictionary
+		optional_ptr<ArrowArray> arrow_dictionary = nullptr;
 
-	vector<idx_t> projection_ids;
-	vector<LogicalType> scanned_types;
+		// Cache the (optional) dictionary of this array
+		unique_ptr<Vector> dictionary;
 
-	idx_t MaxThreads() const override {
-		return max_threads;
-	}
+		//! Run-end-encoding state
+		ArrowRunEndEncodingState run_end_encoding;
 
-	bool CanRemoveFilterColumns() const {
-		return !projection_ids.empty();
-	}
-};
+		//! Reference to duckdb context
+		ClientContext &context;
 
-struct ArrowTableFunction {
-public:
-	static void RegisterFunction(BuiltinFunctions &set);
+    // Constructors
+		explicit ArrowArrayScanState(ArrowScanLocalState &state, ClientContext &context);
 
-public:
-	//! Binds an arrow table
-	static unique_ptr<FunctionData> ArrowScanBind(ClientContext &context, TableFunctionBindInput &input,
-	                                              vector<LogicalType> &return_types, vector<string> &names);
-	static unique_ptr<FunctionData> ArrowScanBindDumb(ClientContext &context, TableFunctionBindInput &input,
-	                                                  vector<LogicalType> &return_types, vector<string> &names);
-	//! Actual conversion from Arrow to DuckDB
-	static void ArrowToDuckDB(ArrowScanLocalState &scan_state, const arrow_column_map_t &arrow_convert_data,
-	                          DataChunk &output, idx_t start, bool arrow_scan_is_projected = true,
-	                          idx_t rowid_column_index = COLUMN_IDENTIFIER_ROW_ID);
+    // Functions
+    ArrowArrayScanState&      GetChild(idx_t child_idx);
+    Vector&                   GetDictionary();
+    ArrowRunEndEncodingState& RunEndEncoding() { return run_end_encoding; }
 
-	//! Get next scan state
-	static bool ArrowScanParallelStateNext(ClientContext &context, const FunctionData *bind_data_p,
-	                                       ArrowScanLocalState &state, ArrowScanGlobalState &parallel_state);
+    bool HasDictionary() const;
+    bool CacheOutdated(ArrowArray *dictionary) const;
+    void AddDictionary(unique_ptr<Vector> dictionary_p, ArrowArray *arrow_dict);
 
-	//! Initialize Global State
-	static unique_ptr<GlobalTableFunctionState> ArrowScanInitGlobal(ClientContext &context,
-	                                                                TableFunctionInitInput &input);
+    void Reset() {
+      // Note: dictionary is not reset
+      // the dictionary should be the same for every array scanned of this column
+      run_end_encoding.Reset();
+      for (auto &child : children) { child.second->Reset(); }
+      owned_data.reset();
+    }
+  };
 
-	//! Initialize Local State
-	static unique_ptr<LocalTableFunctionState> ArrowScanInitLocalInternal(ClientContext &context,
-	                                                                      TableFunctionInitInput &input,
-	                                                                      GlobalTableFunctionState *global_state);
-	static unique_ptr<LocalTableFunctionState> ArrowScanInitLocal(ExecutionContext &context,
-	                                                              TableFunctionInitInput &input,
-	                                                              GlobalTableFunctionState *global_state);
+  struct ArrowScanLocalState : public LocalTableFunctionState {
+    //! Type alias for templated unordered_map containing indexed "Scan states"
+    using ScanStateMap = unordered_map<idx_t, unique_ptr<ArrowArrayScanState>>;
 
-	//! Scan Function
-	static void ArrowScanFunction(ClientContext &context, TableFunctionInput &data, DataChunk &output);
-	static void PopulateArrowTableType(DBConfig &config, ArrowTableType &arrow_table,
-	                                   const ArrowSchemaWrapper &schema_p, vector<string> &names,
-	                                   vector<LogicalType> &return_types);
+    shared_ptr<ArrowArrayWrapper>       chunk;
+    vector<column_t>                    column_ids;
+    ScanStateMap                        array_states;
+    idx_t                               chunk_offset { 0 };
+    idx_t                               batch_index  { 0 };
+    TableFilterSet*                     filters      { nullptr };
 
-protected:
-	//! Defines Maximum Number of Threads
-	static idx_t ArrowScanMaxThreads(ClientContext &context, const FunctionData *bind_data);
+    //! The DataChunk containing all read columns (even filter columns that are immediately removed)
+    DataChunk all_columns;
+    ClientContext &context;
 
-	//! Allows parallel Create Table / Insertion
-	static OperatorPartitionData ArrowGetPartitionData(ClientContext &context, TableFunctionGetPartitionInput &input);
+    // constructors
+    explicit ArrowScanLocalState(unique_ptr<ArrowArrayWrapper> current_chunk, ClientContext &context)
+      : chunk(current_chunk.release()), context(context) {}
 
-	//! Specify if a given type can be pushed-down by the arrow engine
-	static bool ArrowPushdownType(const LogicalType &type);
-	//! -----Utility Functions:-----
-	//! Gets Arrow Table's Cardinality
-	static unique_ptr<NodeStatistics> ArrowScanCardinality(ClientContext &context, const FunctionData *bind_data);
-	//! Gets the progress on the table scan, used for Progress Bars
-	static double ArrowProgress(ClientContext &context, const FunctionData *bind_data,
-	                            const GlobalTableFunctionState *global_state);
-};
+    // functions
+    void Reset() {
+      chunk_offset = 0;
+      for (auto &col : array_states) { col.second->Reset(); }
+    }
+
+    ArrowArrayScanState& GetState(idx_t child_idx) {
+      auto it = array_states.find(child_idx);
+
+      if (it == array_states.end()) {
+        auto  child_p = make_uniq<ArrowArrayScanState>(*this);
+        auto& child   = *child_p;
+        array_states.emplace(child_idx, std::move(child_p));
+
+        return child;
+      }
+
+      return *it->second;
+    }
+
+  };
+
+	struct ArrowScanGlobalState : public GlobalTableFunctionState {
+		unique_ptr<ArrowArrayStreamWrapper> stream;
+
+		mutex main_mutex;
+		idx_t max_threads { 1 };
+		idx_t batch_index { 0 };
+		bool  done        { false };
+
+		vector<idx_t>       projection_ids;
+		vector<LogicalType> scanned_types;
+
+		idx_t MaxThreads()             const override { return max_threads; }
+		bool  CanRemoveFilterColumns() const          { return !projection_ids.empty(); }
+	};
+
+	struct ArrowTableFunction {
+		// Public API
+		static void RegisterFunction(BuiltinFunctions &set);
+
+		//! Binds an arrow table
+		static unique_ptr<FunctionData>
+		ArrowScanBind( ClientContext&          context
+		              ,TableFunctionBindInput& input
+		              ,vector<LogicalType>&    return_types
+		              ,vector<string>&         names);
+
+		//! Binds an arrow table
+		static unique_ptr<FunctionData>
+		ArrowScanBindDumb( ClientContext&          context
+		                  ,TableFunctionBindInput& input
+		                  ,vector<LogicalType>&    return_types
+		                  ,vector<string>&         names);
+
+		//! Actual conversion from Arrow to DuckDB
+		static void
+		ArrowToDuckDB( ArrowScanLocalState&      scan_state
+		              ,const arrow_column_map_t& arrow_convert_data
+		              ,DataChunk&                output
+		              ,idx_t                     start
+		              ,bool                      arrow_scan_is_projected = true
+		              ,idx_t                     rowid_column_index = COLUMN_IDENTIFIER_ROW_ID);
+
+		//! Get next scan state
+		static bool
+		ArrowScanParallelStateNext( ClientContext&        context
+		                           ,const FunctionData*   bind_data_p
+		                           ,ArrowScanLocalState&  state
+		                           ,ArrowScanGlobalState& parallel_state);
+
+		//! Initialize Global State
+		static unique_ptr<GlobalTableFunctionState>
+		ArrowScanInitGlobal(ClientContext &context, TableFunctionInitInput &input);
+
+		//! Initialize Local State
+		static unique_ptr<LocalTableFunctionState>
+		ArrowScanInitLocalInternal( ClientContext&            context
+		                           ,TableFunctionInitInput&   input
+		                           ,GlobalTableFunctionState* global_state);
+
+		static unique_ptr<LocalTableFunctionState>
+		ArrowScanInitLocal( ExecutionContext&         context
+		                   ,TableFunctionInitInput&   input
+		                   ,GlobalTableFunctionState* global_state);
+
+		//! Scan Function
+		static void ArrowScanFunction(ClientContext &context, TableFunctionInput &data, DataChunk &output);
+
+		static void PopulateArrowTableType( DBConfig&            config
+		                                   ,ArrowTableType&      arrow_table
+		                                   ,ArrowSchemaWrapper&  schema_p
+		                                   ,vector<string>&      names
+		                                   ,vector<LogicalType>& return_types);
+
+	 protected:
+		//! Defines Maximum Number of Threads
+		static idx_t ArrowScanMaxThreads(ClientContext &context, const FunctionData *bind_data);
+
+		//! Allows parallel Create Table / Insertion
+		static OperatorPartitionData ArrowGetPartitionData(ClientContext &context, TableFunctionGetPartitionInput &input);
+
+		//! Specify if a given type can be pushed-down by the arrow engine
+		static bool ArrowPushdownType(const LogicalType &type);
+
+		//! -----Utility Functions:-----
+		//! Gets Arrow Table's Cardinality
+		static unique_ptr<NodeStatistics> ArrowScanCardinality(ClientContext &context, const FunctionData *bind_data);
+
+		//! Gets the progress on the table scan, used for Progress Bars
+		static double ArrowProgress( ClientContext &context
+		                            ,const FunctionData *bind_data
+		                            ,const GlobalTableFunctionState *global_state);
+	};
 
 } // namespace duckdb
