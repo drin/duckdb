201a116c1dce90d10fcbec1d85c4c69cd3ee068e
Aldrin Montana <octalene.dev@pm.me>
2024-10-29 10:34:12 -0700

    feat: adding substrait TableFunctions
    Added convenience functions to invoke new substrait TableFunctions
    defined in my duckdb-substrait extension.
    
    Also added a few bindings to the python bindings.

--- src/include/duckdb/main/connection.hpp
+++ src/include/duckdb/main/connection.hpp
@@ -171,6 +171,24 @@ public:
 	DUCKDB_API shared_ptr<Relation> RelationFromQuery(unique_ptr<SelectStatement> select_stmt,
 	                                                  const string &alias = "queryrelation", const string &query = "");
 
+  // >> Substrait related functions were removed from upstream
+
+	//! Returns a substrait BLOB from a valid query
+	DUCKDB_API string GetSubstrait(const string &query);
+	//! Returns a Query Result from a substrait blob
+	DUCKDB_API unique_ptr<QueryResult> FromSubstrait(const string &proto);
+	//! Returns a substrait BLOB from a valid query
+	DUCKDB_API string GetSubstraitJSON(const string &query);
+	//! Returns a Query Result from a substrait JSON
+	DUCKDB_API unique_ptr<QueryResult> FromSubstraitJSON(const string &json);
+
+	// >> Mohair functions
+	//! Returns an explain plan for a substrait blob (using mohair path and extensions)
+	DUCKDB_API unique_ptr<QueryResult> TranslateMohair(const string &proto);
+
+	//! Returns a Query Result from a substrait blob (using mohair path and extensions)
+	DUCKDB_API unique_ptr<QueryResult> ExecuteMohair(const string &proto);
+
 	DUCKDB_API void BeginTransaction();
 	DUCKDB_API void Commit();
 	DUCKDB_API void Rollback();
